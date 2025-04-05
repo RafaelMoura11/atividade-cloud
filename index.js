fb40e4dc22f6a875c083c5dae961dee7327bb6a7
Rafael Moura <rafaelmoura7033@gmail.com>
2025-04-05 18:03:46 -0300

    Modificando o arquivo index.js para ver se a aplicação volta

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-const express = require("express");
-const sequelize = require("./config/db");
-require("dotenv").config();
-require("./models/Cliente");
-require("./models/Veiculo");
-require("./models/Servico");
-require("./models/OrdemServico");
-require("./models/OrdemServicoServico");
-
+const express = require('express');
 const app = express();
-app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// Rota simples para testar
-app.get("/", (req, res) => {
-  res.send("API da Oficina rodando!");
+app.get('/', (req, res) => {
+  res.send('Está funcionando');
 });
 
-// Sincronizar DB e subir o servidor
-sequelize.sync({ alter: true }).then(() => {
-  console.log(`Banco sincronizado em ${process.env.DB_HOST}`);
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-  });
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
