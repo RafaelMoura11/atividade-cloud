1262ee380ed8643c6b185ec5b0b06443770e4d84
Rafael Moura <rafaelmoura7033@gmail.com>
2025-04-05 15:33:08 -0300

    Configurando index.js e package.json

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const express = require('express');
-const mysql = require('mysql2');
-require('dotenv').config();
-
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
 app.get('/ping', (req, res) => {
-  db.query('SELECT 1 + 1 AS result', (err, results) => {
-    if (err) {
-      return res.status(500).send('Erro ao conectar ao banco de dados');
-    }
-    res.json({ message: 'pong', db_result: results[0].result });
-  });
+  res.send('pong');
 });
 
 app.listen(port, () => {
-  console.log(`API rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
