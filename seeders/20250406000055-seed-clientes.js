b8a619c77b0b7410e024986e407b747c7428abfb
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2025-04-08 19:17:09 -0300

    Update 20250406000055-seed-clientes.js

--- seeders/20250406000055-seed-clientes.js
+++ seeders/20250406000055-seed-clientes.js
@@ -4,16 +4,37 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('clientes', [
       {
-        nome: 'João Silva',
+        nome: 'Rafael Moura',
         telefone: '11999999999',
-        email: 'joao@email.com',
+        email: 'rafael@email.com',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        nome: 'Maria Oliveira',
+        nome: 'Amália Nascimento',
         telefone: '11888888888',
-        email: 'maria@email.com',
+        email: 'amalia@email.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nome: 'Amanda Aguiar',
+        telefone: '11888888888',
+        email: 'amanda@email.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nome: 'Luana Martins',
+        telefone: '11888888888',
+        email: 'luana@email.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nome: 'Abraão Saraiva',
+        telefone: '11888888888',
+        email: 'abraao@email.com',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
